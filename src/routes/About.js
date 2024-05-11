b527da3d0b91f3bcbbd1154ceee4eb430d663c74
Anita Sharma <sharma.anita00001@gmail.com>
2024-05-11 07:46:55 +0545

    Add Line Divider on Home and About Page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import AboutPhoto from '../components/AboutPhoto';
 import AboutDescription from '../components/AboutDescription';
+import LineDivider from '../components/LineDivider';
 
 const About = () => {
   return (
-    <div id="about" className="min-h-screen bg-color4 grid grid-cols-2 gap-4 px-20">
-      <div className="photo">< AboutPhoto /></div>
-      <div classname="about-description">< AboutDescription /></div>
-    </div>
+    <>
+      <div id="about" className="min-h-screen bg-color4 grid grid-cols-2 gap-4 px-20">
+        <div className="photo">< AboutPhoto /></div>
+        <div classname="about-description">< AboutDescription /></div>
+      </div>
+      < LineDivider />
+    </>
   );
 };
 
