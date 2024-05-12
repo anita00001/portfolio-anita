31e7b9df8848fe9b98c87c90ba1e7938c215e189
Anita Sharma <sharma.anita00001@gmail.com>
2024-05-12 15:09:07 +0545

    Add Initial State for the Services

--- src/redux/Services/serviceSlice.js
+++ src/redux/Services/serviceSlice.js
@@ -3,7 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const serviceSlice = createSlice({
     name: "service",
     initialState: {
-        service: [],
+        service: [
+            { id: 1, name: 'Service 1', description: 'Description for Service 1' },
+            { id: 2, name: 'Service 2', description: 'Description for Service 2' },
+        ],
     },
     reducers: {},
 });
