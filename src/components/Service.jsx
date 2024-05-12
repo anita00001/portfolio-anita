4750b92929d4f7a2e2508d8d3ea968cd086b82af
Anita Sharma <sharma.anita00001@gmail.com>
2024-05-12 14:51:32 +0545

    Add Service component layout

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GrServices } from "react-icons/gr";
 
 const Service = () => {
   return (
@@ -7,11 +8,15 @@ const Service = () => {
         <h1 className="about text-4xl font-bold py-10 font-crete text-gradient">
           Services
         </h1>
-        <div className="grid grid-cols-5 gap-4">
+        
+        <div className="services grid grid-cols-5 gap-4">
           <div className="icon col-span-1">
             icon
           </div>
-          <div className="service-name col-span-4">service name</div>
+          <div className="service-name col-span-4">
+            <h4 className='text-bold'>service name</h4>
+            <p className='text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero neque in quasi voluptates ratione recusandae sint deleniti velit dicta odio!</p>
+          </div>
         </div>
       </div>
     </>
