1abe3acb9b79672c8f79b1804e2d1191bf8bc643
Anita Sharma <sharma.anita00001@gmail.com>
2024-05-12 16:31:57 +0545

    Add search bar in service list

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+
 import { GrServices } from "react-icons/gr";
 
 const Service = () => {
   const serviceList = useSelector((state) => state.service.service);
+
+  const [searchQuery, setSearchQuery] = useState('');
   const [expandedServiceId, setExpandedServiceId] = useState(null);
+  const [showAllServices, setShowAllServices] = useState(false);
+  
+  const initialServicesToShow = 4;
+
+  const filteredServices = serviceList.filter((service) =>
+    service.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const visibleServices = showAllServices ? filteredServices : filteredServices.slice(0, initialServicesToShow);
+
+  const toggleShowAllServices = () => {
+    setShowAllServices((prevShowAllServices) => !prevShowAllServices);
+  };
 
   const handleServiceClick = (id) => {
     setExpandedServiceId(id === expandedServiceId ? null : id);
@@ -16,8 +32,19 @@ const Service = () => {
         <h1 className="service text-4xl font-bold py-10 font-crete text-gradient">
           Services
         </h1>
+
+        <div className="search-bar mb-4">
+          <input
+            type="text"
+            placeholder="Search Services..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full px-4 py-2 border border-color2 rounded-lg focus:outline-none focus:border-color1"
+          />
+        </div>
+
         <div className='service-list'>
-          {serviceList.map((service) => (
+          {visibleServices.map((service) => (
             <div
             className="services grid grid-cols-5 gap-4 p-4 mb-4 border border-color2 bg-color1 hover:bg-color6 hover:border-color1"
             key={service.id}
@@ -39,6 +66,11 @@ const Service = () => {
             </div>
           ))}
         </div>
+        {showAllServices ? (
+          <button className="text-xl mt-4 p-4 text-justify border border-color2 flex items-center bg-color1 hover:bg-color6 hover:border-color1 text-white" onClick={toggleShowAllServices}>See Less Services</button>
+        ) : (
+          <button className="text-xl mt-4 p-4 text-justify border border-color2 flex items-center bg-color1 hover:bg-color6 hover:border-color1 text-white" onClick={toggleShowAllServices}>See More Services</button>
+        )}
       </div>
     </>
   );
