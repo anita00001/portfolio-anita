1942df53dd4c8386a944cc5d3e57e2d7749c76a7
Anita Sharma <sharma.anita00001@gmail.com>
2024-05-12 16:00:35 +0545

    Display Services in react component

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { GrServices } from "react-icons/gr";
 
 const Service = () => {
+  const serviceList = useSelector((state) => state.service.service);
+  const [expandedServiceId, setExpandedServiceId] = useState(null);
+
+  const handleServiceClick = (id) => {
+    setExpandedServiceId(id === expandedServiceId ? null : id);
+  };
+
   return (
     <>
-      <div className="about-description p-20">
-        <h1 className="about text-4xl font-bold py-10 font-crete text-gradient">
+      <div className="about-service p-20">
+        <h1 className="service text-4xl font-bold py-10 font-crete text-gradient">
           Services
         </h1>
-        
-        <div className="services grid grid-cols-5 gap-4">
-          <div className="icon col-span-1">
-            icon
-          </div>
-          <div className="service-name col-span-4">
-            <h4 className='text-bold'>service name</h4>
-            <p className='text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero neque in quasi voluptates ratione recusandae sint deleniti velit dicta odio!</p>
-          </div>
+        <div className='service-list'>
+          {serviceList.map((service) => (
+            <div
+            className="services grid grid-cols-5 gap-4 p-4 mb-4 border border-color2 bg-color1 hover:bg-color6 hover:border-color1"
+            key={service.id}
+            onClick={() => handleServiceClick(service.id)}
+            >
+              <div
+                className="icon col-span-1 border border-color2 flex items-center justify-center h-16"
+              >
+                < GrServices />
+              </div>
+              <div className="service-name col-span-4">
+                <h4 className='text-bold'>{service.name}</h4>
+                <p
+                  className={`text-justify ${expandedServiceId === service.id ? '' : 'line-clamp-1'}`}
+                >
+                  {service.description}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- src/redux/Services/serviceSlice.js
+++ src/redux/Services/serviceSlice.js
@@ -4,8 +4,12 @@ const serviceSlice = createSlice({
     name: "service",
     initialState: {
         service: [
-            { id: 1, name: 'Service 1', description: 'Description for Service 1' },
-            { id: 2, name: 'Service 2', description: 'Description for Service 2' },
+            { id: 1, name: 'Full Stack Development', description: 'Full Stack Development with React and Rails' },
+            { id: 2, name: 'Backend Development', description: 'Backend Development with Ruby and Ruby on Rails' },
+            { id: 3, name: 'Frontend Development', description: 'Frontend Development with React/Redux, Javascript, Bootstrap, TailwindCSS, HTML and CSS3' },
+            { id: 4, name: 'SEO Optimization', description: 'Search Engine Optimization of the website' },
+            { id: 5, name: 'Software Testing', description: 'Code Testing with JEST' },
+            { id: 6, name: 'Database Management', description: 'Postgres SQL' },
         ],
     },
     reducers: {},
