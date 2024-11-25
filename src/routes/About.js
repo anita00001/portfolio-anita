38bc67bedb661884de2db079b405378cbb46af1a
Anita Sharma <sharma.anita00001@gmail.com>
2024-11-25 11:16:58 +0545

    Modify About

--- src/routes/About.js
+++ src/routes/About.js
@@ -10,21 +10,23 @@ import Education from '../components/Education';
 const About = () => {
   return (
     <>
-      <div id="about" className="min-h-screen bg-color4 grid grid-cols-2 gap-4 px-20">
+    <div id="about">
+      <div id="about-me" className="min-h-screen bg-color4 grid grid-cols-2 gap-4 px-20">
         <div className="photo">< AboutPhoto /></div>
         <div classname="about-description">< AboutDescription /></div>
       </div>
-      < LineDivider />
+      {/* < LineDivider /> */}
       <div id="service-skill" className="min-h-screen bg-color4 grid grid-cols-2 gap-4 px-20">
         <div className="about-service">< Service /></div>
         <div classname="about-skill">< Skill /></div>
       </div>
-      < LineDivider />
+      {/* < LineDivider /> */}
       <div id="experience-education" className="min-h-screen bg-color4 grid grid-cols-2 gap-4 px-20">
         <div className="about-experience">< Experience /></div>
         <div classname="about-education">< Education /></div>
       </div>
       < LineDivider />
+      </div>
     </>
   );
 };
