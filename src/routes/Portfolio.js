7b67e47ee61203f6ca314b15d60a71c22379d194
Anita Sharma <sharma.anita00001@gmail.com>
2024-11-25 10:09:53 +0545

    Modify featured-project

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -7,9 +7,7 @@ import PortfolioProject from '../components/PortfolioProject';
 const Portfolio = () => {
   return (
     <>
-      {/* <div id="featured-project" className="min-h-screen bg-color4 grid grid-cols-2 gap-4 px-20"> */}
-        <div className="featured-photo"><FeaturedProject /></div>
-      {/* </div> */}
+      <div className="featured-photo"><FeaturedProject /></div>
       <div id="portfolio-projects" className="min-h-screen bg-color4 px-20 py-10">
         <PortfolioProject />
       </div>
